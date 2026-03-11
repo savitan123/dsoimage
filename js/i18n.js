3fbcd618d579a7e3e1c43cd39e40d9ed7e050744
Shimon Avitan <avitanshimon@gmail.com>
2026-03-11 16:18:39 +0200

    Fix carousel flexbox RTL bugs with exact pixel translation and use SVGs for USA and Israel flags

--- js/i18n.js
+++ js/i18n.js
@@ -53,11 +53,12 @@
             htmlEl.setAttribute('lang', 'en');
         }
 
-        // Update toggle button label with flag emoji
+        // Update toggle button label with SVG flag
         const toggleBtn = document.getElementById('lang-toggle-btn');
         if (toggleBtn) {
-            // Show the flag of the language you'd SWITCH TO
-            toggleBtn.textContent = lang === 'he' ? '🇺🇸 EN' : '🇮🇱 עב';
+            const flagUSA = `<img src="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 40'><path fill='%23fff' d='M0 0h60v40H0z'/><path fill='%23c41130' d='M0 0h60v3.07H0zm0 6.15h60v3.08H0zm0 6.16h60v3.08H0zm0 6.15h60v3.08H0zm0 6.16h60v3.07H0zm0 6.15h60v3.08H0zm0 6.15h60v3.08H0z'/><path fill='%23002164' d='M0 0h24v21.54H0z'/><g fill='%23fff'><g id='d'><g id='c'><g id='e'><g id='b'><path id='a' d='M1.2 1.9l.4 1.2 1.1-.9-1.4-.9-1.4.9 1.1.9z'/><use href='%23a' y='3.84'/><use href='%23a' y='7.69'/><use href='%23a' y='11.54'/><use href='%23a' y='15.38'/></g><use href='%23b' x='4'/><use href='%23b' x='8'/><use href='%23b' x='12'/><use href='%23b' x='16'/><use href='%23b' x='20'/></g><use href='%23a' x='2' y='1.92'/><use href='%23a' x='2' y='5.77'/><use href='%23a' x='2' y='9.62'/><use href='%23a' x='2' y='13.46'/><use href='%23a' x='2' y='17.31'/></g><use href='%23c' x='4'/></g><use href='%23d' x='8'/><use href='%23d' x='16'/></g></svg>" style="width:20px;height:14px;vertical-align:middle;margin-right:5px;display:inline-block"> EN`;
+            const flagIL = `<img src="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 40'><path fill='%23fff' d='M0 0h60v40H0z'/><path fill='%230038b8' d='M0 4h60v6H0zm0 24h60v6H0z'/><g fill='none' stroke='%230038b8' stroke-width='2.5'><path d='M30 11l-8 13.8h16z'/><path d='M30 29l-8-13.8h16z'/></g></svg>" style="width:20px;height:14px;vertical-align:middle;margin-right:5px;display:inline-block"> עב`;
+            toggleBtn.innerHTML = lang === 'he' ? flagUSA : flagIL;
         }
 
         // Save preference
